7ecaafc552d1fbdf305e8b85dd4a3b7cdbb7ecde
XAprendizSenaX <aprendizsenancs@gmail.com>
2025-08-05 14:48:18 -0500

    modificacion agregar

--- agregar_producto.php
+++ agregar_producto.php
@@ -1,13 +1,35 @@
 <?php
-// editar_producto.php
+// editar_producto.php  
 // Este script permite editar un producto de la tabla 'papeleria'
 include 'includes/db.php';
 include 'includes/header.php';
 
 $mensaje = '';
-$producto = null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Recolectar datos del formulario
+    $codigo = $_POST['CODIGO'];
+    $producto = $_POST['PRODUCTO'];
+    $cant = $_POST['CANT'];
+    $unidad = $_POST['UNIDAD'];
+
+    if (!empty($producto) && !empty($cant) && !empty ($unidad)) {
+    //Preparar la consulta SQL para actualizar el producto
+        $sql = 'INSERT INTO papeleria (PRODUCTO, CANT, UNIDAD) VALUES (?,?,?,?)';
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ssis', $codigo, $producto, $cant, $unidad);
 
 
+        if ($stmt->execute()) {
+            $mensaje = "<p class='btn-success'>Producto actualizado correctamente.</p>";
+        } else {
+            $mensaje = "<p class='btn-danger'>Error al actualizar el producto: " . $stmt->error . "</p>";
+        }
+        $stmt->close();
+    } else {
+        $mensaje = "<div class='alert alert-warning'>⚠️ Por favor, complete todos los campos.</div>";
+    }
+}
 ?>
 
 <h2>Editar Producto</h2>
