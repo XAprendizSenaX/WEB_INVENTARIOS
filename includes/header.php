edd1da04c650ad627d4c6e0c9d21d3b376571ecf
XAprendizSenaX <aprendizsenancs@gmail.com>
2025-08-05 16:42:15 -0500

    logo incluido

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgcW54AkpoPfFPQacImJCIwpJEctdfJh4t0g&s" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styleH.css">
-    <title>Sistema de Inventario</title>
+    <title>NCS INVENTARIO</title>
 </head>
 <body>
     <div class="container">
