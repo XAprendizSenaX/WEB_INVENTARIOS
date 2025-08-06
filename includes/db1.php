2aec4e0310bbc31705aad4993074a4e06cfe8481
XAprendizSenaX <aprendizsenancs@gmail.com>
2025-08-06 16:42:46 -0500

    demo nube sql

--- includes/db1.php
+++ includes/db1.php
@@ -1,12 +1,13 @@
-<?php
+<?php 
 
-$host = 'localhost';
-$db = 'inventario_db';
-$user = 'root';
-$password = '';
+$host = 'db.jryuieelkfclwwoatnlk.supabase.co';
+$db = 'postgres';
+$user = 'postgres';
+$password = 'NCSdb2025@_';
+$port = '5432';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "pgsql:host=$host;port=$port;dbname=$db";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -14,8 +15,11 @@ $options = [
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $password, $options);
+    $pdo = new PDO ($dsn, $user, $password, $options);
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
-?>  
+
+?>
+
+
