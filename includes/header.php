1e4ecccba0885529d5344ab98568858e04e12220
XAprendizSenaX <aprendizsenancs@gmail.com>
2025-08-26 15:15:09 -0500

    Actualizacion encabezado

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgcW54AkpoPfFPQacImJCIwpJEctdfJh4t0g&s" type="image/png">
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgcW54AkpoPfFPQacImJCIwpJEctdfJh4t0g&s"
+        type="image/png">
     <title>Inventario NCS</title>
     <!-- Incluir Bootstrap CSS para un diseño limpio y responsive -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -53,20 +54,22 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
     <div class="header container">
         <h1>Sistema de Inventario NCS</h1>
-        <nav>
-            <ul>
-                <il><a href="index.php">Inicio</a></li>
-                    <li><a href="producto_categoria.php">Productos</a></li>
-                    <li><a href="categorias.php">Categorías</a></li>
-                    <li><a href="reportes.php">Reportes</a></li>
-                    <li><a href="buscar_producto.php">Buscar</a></li>
-            </ul>
-        </nav>
+
         <?php
         // Solo muestra el botón de cerrar sesión si el usuario está logueado
         // Y no está en la página de login
         if (isset($_SESSION['user_id']) && $current_page !== 'login.php') {
-            echo '<a href="logout.php" class="btn btn-danger btn-logout">Cerrar Sesión</a>';
+            echo '
+            <nav>
+                <ul>
+                    <il><a href="index.php">Inicio</a></li>
+                    <li><a href="producto_categoria.php">Productos</a></li>
+                    <li><a href="categorias.php">Categorías</a></li>
+                    <li><a href="reportes.php">Reportes</a></li>
+                    <li><a href="buscar_producto.php">Buscar</a></li>
+                </ul>
+            </nav>
+            <a href="logout.php" class="btn btn-danger btn-logout">Cerrar Sesión</a>';
         }
         ?>
     </div>
